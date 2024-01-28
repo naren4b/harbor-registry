49bcf5debda7e11aa5bab20770e21d64b36146b4
Narendranath Panda <3488520+naren4b@users.noreply.github.com>
2024-01-28 08:09:57 +0530

    Create create-project.sh

---
+++ harbor-api/create-project.sh
@@ -0,0 +1,19 @@
+cat<<EOF >my-project.json
+{
+  "project_name": "naren",
+  "public": "false",
+  "metadata": {
+    "public": false
+  }
+ "registry_id": 0
+}
+EOF
+
+
+curl -k -s \
+        -X POST \
+        -u ${ADMIN_USER}:${ADMIN_PASSWORD} \
+        -H 'accept: application/json' \
+        -H 'Content-Type: application/json' \
+        "https://${HARBOR_URL}/api/v2.0/projects" \
+        -d @my-project.json
