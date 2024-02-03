2ed77ce26accdb6ddbb4660e42b77808ba841ed0
Narendranath Panda <narendranath.panda@gmail.com>
2024-02-03 20:01:01 +0530

    my notes

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-# Install Ingrss controller
-wget https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-kubectl label nodes controlplane ingress-ready="true"
-kubectl apply -f deploy.yaml
-
 # Install harbor
 helm upgrade --install habor harbor/harbor \
     -f harbor-values.yaml \
